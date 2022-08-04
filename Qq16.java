17ae4cf1d4998ff254c1672be08ed7640801d1d1
Amlesh103 <amleshkumarshukla@gmail.com>
2022-08-04 20:42:53 +0530

    Commiting Qq16.java file

---
+++ Qq16.java
@@ -0,0 +1,8 @@
+public class qq16{
+public static void main(String[] args){
+StringBuffer sb = new StringBuffer("a");
+sb.append("12345678912345678");
+System.out.println(sb.length());
+System.out.println(sb.capacity());
+}
+}
