a1da79f4b8347c6171e9f26366d5839183ee2343
Amlesh103 <amleshkumarshukla@gmail.com>
2022-08-04 20:50:32 +0530

    Commiting with Author name

--- Company.java
+++ Company.java
@@ -1,3 +1,5 @@
+//Author: Amlesh
+
 class Employee
 {
 private int empid;
@@ -21,3 +23,46 @@ e.setEmpid(101);
 System.out.println(e.getEmpid());
 }
 }
+
+package qaproject;
+
+public class Block {
+	int width;
+	int length;
+	int height;
+	int[] arr= {width, length, height};
+
+	// int[] arr = {width, length, height};
+	//{
+	  //int arr[] = new int[]{width, length, height};
+	  
+	//}
+	  public Block(int[] arr) {
+		// TODO Auto-generated constructor stub
+		  this.arr=arr;
+	  }
+		/*this.width = width;
+	  this.length = length;
+	  this.height = height;*/
+	  //Block b = new Block();
+	    int getWidth(int width){
+	      	     
+		return this.width=width;
+	    }
+	    public int getLength(int length)
+	      {
+	      return this.length=length;
+	      }
+	  public int getHeight(int height)
+	    {
+	    return this.height=height;
+	  }
+	  public int getVolume()
+	    {
+	    return width*length*height;
+	  }
+	  public int getSurfaceArea()
+	    {
+	    return 6*length*length;
+	  }
+	  }
